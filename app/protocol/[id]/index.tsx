281cbf12935c53281ce6d131ec49e322b1ac0938
Danny Tunney <dtun@me.com>
2025-10-09 14:58:31 -0700

    refactor(protocol): use flatlist

--- app/protocol/[id]/index.tsx
+++ app/protocol/[id]/index.tsx
@@ -1,16 +1,25 @@
 import { protocolMap } from "@/constants/protocol";
 import { usePathname } from "expo-router";
-import { Text, View } from "react-native";
+import { FlatList, Text, View } from "react-native";
 
 export default function ProtocolScreen() {
   let id = usePathname().split("/").pop();
   let protocol = protocolMap[id as keyof typeof protocolMap];
 
   return (
-    <View>
-      <Text>{protocol.focus}</Text>
-      <Text>{protocol.description.join("\n\n")}</Text>
-      <Text>{protocol.exerciseExamples.join("\n\n")}</Text>
-    </View>
+    <FlatList
+      data={[
+        {
+          render: (
+            <View>
+              <Text>{protocol.focus}</Text>
+              <Text>{protocol.description.join("\n\n")}</Text>
+              <Text>{protocol.exerciseExamples.join("\n\n")}</Text>
+            </View>
+          ),
+        },
+      ]}
+      renderItem={({ item }) => item.render}
+    />
   );
 }
