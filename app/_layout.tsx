0b10281cac8dce7f60985b2077b047fc0f2af8ed
Danny Tunney <dtun@me.com>
2025-10-10 08:49:54 -0700

    feat(styles): add theme

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,10 @@
+import { config } from "@/tamagui.config";
 import {
   DarkTheme,
   DefaultTheme,
   ThemeProvider,
 } from "@react-navigation/native";
+import { TamaguiProvider } from "@tamagui/core";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import "react-native-reanimated";
@@ -18,22 +20,24 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen
-          name="modal"
-          options={{ presentation: "modal", title: "Modal" }}
-        />
-        <Stack.Screen
-          name="protocol/[id]/index"
-          options={{ title: "Protocol", headerBackTitle: "Back" }}
-        />
-        <Stack.Screen
-          name="settings"
-          options={{ title: "Settings", headerBackTitle: "Back" }}
-        />
-      </Stack>
-      <StatusBar style="auto" />
+      <TamaguiProvider config={config}>
+        <Stack>
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+          <Stack.Screen
+            name="modal"
+            options={{ presentation: "modal", title: "Modal" }}
+          />
+          <Stack.Screen
+            name="protocol/[id]/index"
+            options={{ title: "Protocol", headerBackTitle: "Back" }}
+          />
+          <Stack.Screen
+            name="settings"
+            options={{ title: "Settings", headerBackTitle: "Back" }}
+          />
+        </Stack>
+        <StatusBar style="auto" />
+      </TamaguiProvider>
     </ThemeProvider>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,14 @@
 import { protocolMap } from "@/constants/protocol";
 import { Link } from "expo-router";
-import { FlatList, Text, View } from "react-native";
+import { FlatList } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { Card, Text } from "tamagui";
 
 export default function HomeScreen() {
   return (
     <SafeAreaView style={{ padding: 8 }}>
-      <Link href="/settings" asChild>
-        <Text style={{}}>Settings</Text>
-      </Link>
       <FlatList
+        contentContainerStyle={{ gap: 8 }}
         data={
           [
             { day: "Sunday", protocol: "1" },
@@ -21,20 +20,25 @@ export default function HomeScreen() {
             { day: "Saturday", protocol: "7" },
           ] as const
         }
+        ListHeaderComponent={
+          <Link href="/settings" asChild>
+            <Text style={{}}>Settings</Text>
+          </Link>
+        }
         renderItem={({ item }) => (
           <Link href={`/protocol/${item.protocol}`}>
-            <View
-              style={[
-                {
-                  borderColor: isToday(item.day) ? "red" : "transparent",
-                  borderWidth: 4,
-                  width: "100%",
-                },
-              ]}
+            <Card
+              backgroundColor="$background"
+              borderColor={isToday(item.day) ? "$accent1" : "transparent"}
+              borderWidth="$1.5"
+              width="100%"
             >
-              <Text>{item.day}</Text>
-              <Text>{protocolMap[item.protocol].focus}</Text>
-            </View>
+              <Card.Header>
+                <Text fontWeight="bold">{item.day}</Text>
+                <Text fontSize="$2">{protocolMap[item.protocol].focus}</Text>
+              </Card.Header>
+              <Card.Footer></Card.Footer>
+            </Card>
           </Link>
         )}
       />
